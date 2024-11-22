9915df27a367d4910bd2ad47a52d68e07c242bef
joshpalmer122 <joshpalmer122@u.boisestate.edu>
2024-11-22 01:47:53 -0700

    teams create read delete routes

--- app.py
+++ app.py
@@ -15,7 +15,14 @@ app.config['MYSQL_USER'] = 'cs340_palmerj2'
 app.config['MYSQL_PASSWORD'] = '0690' # last 4 of onid
 app.config['MYSQL_DB'] = 'cs340_palmerj2'
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
-mysql = MySQL(app)
+mysql = MySQL(app) 
+'''
+app.config['MYSQL_HOST'] ='localhost'
+app.config['MYSQL_USER'] ='root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = '340testenv'
+app.config['MYSQL_CURSORCLASS'] = "DictCursor"
+''' # localhost db info, commented out 
 
 # ---------- Home Page Routes Start ----------
 @app.route("/")
@@ -77,12 +84,62 @@ def update_player():
     mysql.connection.commit()
 
     return redirect("/players")
-
-    
-    
-
-
-
 # ---------- Players Routes End ----------
+
+
+
+# ---------- Team Routes Start ----------
+@app.route("/teams", methods = ["GET", "POST"])
+def teams():
+    if request.method == "GET":
+        query = "SELECT userName FROM TeamOwners"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        team_owner_names = cur.fetchall()
+        query = "SELECT teamID, teamName, wins, losses, teamFantasyPoints, TeamOwners.userName, Leagues.leagueName FROM Teams LEFT JOIN TeamOwners ON Teams.teamOwnerID = TeamOwners.teamOwnerID LEFT JOIN Leagues ON Teams.leagueID = Leagues.leagueID;"
+        cur.execute(query)
+        teams_data = cur.fetchall()
+
+    if request.method == "POST":
+        teamName = request.form["name"]
+        wins = request.form["wins"]
+        losses = request.form["losses"]
+        fantasyPoints = request.form["teamFantasyPoints"]
+        owner = request.form["teamOwnerName"]
+        league = request.form["leagueName"]
+        query = """
+        INSERT INTO Teams (teamName, wins, losses, teamFantasyPoints, teamOwnerID, leagueID)
+        VALUES (
+            teamName = %s, 
+            %s, 
+            %s, 
+            %s, 
+            (SELECT teamOwnerID FROM TeamOwners WHERE userName = %s), 
+            (SELECT leagueID FROM Leagues WHERE leagueName = %s)
+        );
+        """
+        cur = mysql.connection.cursor()
+        cur.execute(query, (teamName, wins, losses, fantasyPoints, owner, league))
+        mysql.connection.commit()
+        return redirect("/teams")
+    
+    return render_template("teams.j2", team_owner_names=team_owner_names, teams_data = teams_data)
+
+@app.route("/delete_team/<int:id>")
+def delete_team(id):
+    query = "DELETE Teams FROM Teams WHERE teamID = '%s'"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (id,))
+    mysql.connection.commit()
+
+    return redirect("/teams")
+# ---------- Team Routes End ----------
+
+# ---------- Team Owner Routes Start ----------
+@app.route("/team_owners", methods = ["GET", "POST"])
+def team_owners():
+    return render_template("team_owners.j2")
+# ---------- Team Owner Routes End ----------
+
 if __name__ == "__main__":
     app.run(port=1122, debug=True)
